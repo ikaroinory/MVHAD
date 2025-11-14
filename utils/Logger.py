3ee38acaa2f765f68231984521d1da4b2e7f1bd7
Guanyu Hu <ikaroinory@gmail.com>
2025-11-14 15:27:56 +0800

    refactor: Extract log format string to variable

--- utils/Logger.py
+++ utils/Logger.py
@@ -7,14 +7,12 @@ from loguru import logger
 class Logger:
     @staticmethod
     def init(log_name: str | Path = None):
+        format_str = '<g>{time:YYYY-MM-DD HH:mm:ss.SSS}</g> <r>|</r> <level>{level: <8}</level> <r>|</r> {message}'
         logger.remove()
-        logger.add(sys.stdout, format='<g>{time:YYYY-MM-DD HH:mm:ss.SSS}</g> <r>|</r> <level>{level: <8}</level> <r>|</r> {message}')
+        logger.add(sys.stdout, format=format_str)
 
         if log_name is not None:
-            logger.add(
-                log_name,
-                format='<g>{time:YYYY-MM-DD HH:mm:ss.SSS}</g> <r>|</r> <level>{level: <8}</level> <r>|</r> {message}'
-            )
+            logger.add(log_name, format=format_str)
 
     @staticmethod
     def critical(message: str):
